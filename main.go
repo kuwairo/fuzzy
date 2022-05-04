6795a36a1a9d18eeafcccb4ab58091baf96267a7
Ilya S <sewd33p@gmail.com>
2022-05-04 21:21:40 +0700

    CLI prototype

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"fuzzy/search"
+	"log"
+	"strings"
+)
+
+const (
+	defaultSrc      = "The quick brown fox jumps over the lazy dog"
+	defaultPatterns = "teh doug brown"
 )
 
 func main() {
-	results, _ := search.Search(
-		"The house you bought is very nice",
-		[]string{"e", "houze", "very"},
-		nil,
-	)
+	src := flag.String("s", defaultSrc, "search source")
+	patterns := flag.String("p", defaultPatterns, "search patterns")
+	inFile := flag.Bool("f", false, "in-file search")
+	caseInsensitive := flag.Bool("c", false, "case-insensitive search")
+	reverse := flag.Bool("r", false, "reverse search")
+	matchLimit := flag.Int("m", 10, "displayed matches limit")
+	distThreshold := flag.Int("t", 1, "Levenshtein distance threshold")
+	flag.Parse()
 
-	fmt.Println("The house you bought is very nice")
+	// TODO: results output to a file
+
+	patternsSlice := strings.Split(*patterns, " ")
+
+	results, err := search.Search(
+		*src,
+		patternsSlice,
+		&search.Options{
+			InFile:          *inFile,
+			CaseInsensitive: *caseInsensitive,
+			Reverse:         *reverse,
+			MatchLimit:      *matchLimit,
+			DistThreshold:   *distThreshold,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(results)
 }
